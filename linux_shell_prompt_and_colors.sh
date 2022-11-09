308a8afa800f48f651eb874892a33c350d372595
Daniel <dx.zone@gmail.com>
2022-11-09 18:31:08 -0500

    Added new PROMPT_COMMAND in  BASH Prompt with command substitution

--- linux_shell_prompt_and_colors.sh
+++ linux_shell_prompt_and_colors.sh
@@ -127,8 +127,10 @@ $ $RESET"
 
 #######################################
 #
-# BASH Prompt with command substitution  
+# BASH Prompt with command substitution
 PS1="\[\033[0;33m\][\!]\`if [[ \$? = "0" ]]; then echo "\\[\\033[32m\\]"; else echo "\\[\\033[31m\\]"; fi\`[\u.\h: \`if [[ `pwd|wc -c|tr -d " "` > 18 ]]; then echo "\\W"; else echo "\\w"; fi\`]\$\[\033[0m\] "; echo -ne "\033]0;`hostname -s`:`pwd`\007"
+PROMPT_COMMAND='PS1="\[\033[0;33m\][\!]\`if [[ \$? = "0" ]]; then echo "\\[\\033[32m\\]"; else echo "\\[\\033[31m\\]"; fi\`[\u.\h: \`if [[ `pwd|wc -c|tr -d " "` > 18 ]]; then echo "\\W"; else echo "\\w"; fi\`]\$\[\033[0m\] "; echo -ne "\033]0;`hostname -s`:`pwd`\007"'
+
 
 #######################################
 # Defining colors variables for BASH
